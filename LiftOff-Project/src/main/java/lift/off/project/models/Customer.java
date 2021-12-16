92fd396116d08016002dc9e4e2140da91d13e83e
vaishnavi <vaishu.srini15@gmail.com>
2021-12-16 06:15:54 -0600

    worked on Login and Logout

--- LiftOff-Project/src/main/java/lift/off/project/models/Customer.java
+++ LiftOff-Project/src/main/java/lift/off/project/models/Customer.java
@@ -3,6 +3,8 @@ package lift.off.project.models;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
